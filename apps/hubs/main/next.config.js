83878211040a6ebbc7abe2f4fdefa6712f0a4f89
Luigi Perotti <perottilds@gmail.com>
2021-01-15 16:42:16 -0300

    feat: updates next to use their rewrite api instead of proxies

--- apps/hubs/main/next.config.js
+++ apps/hubs/main/next.config.js
@@ -1,6 +1,18 @@
 const withNx = require('@nrwl/next/plugins/with-nx');
 const withCompileNodeModules = require('@moxy/next-compile-node-modules');
+const identity = require('lodash/identity');
 
 module.exports = withCompileNodeModules({
   include: [/@hookform\/resolvers/],
-})(withNx({}));
+})(
+  withNx({
+    async rewrites() {
+      return [
+        process.env.NODE_ENV !== 'production' && {
+          source: '/something/user/:slug*',
+          destination: 'http://localhost:3333/:slug*',
+        },
+      ].filter(identity);
+    },
+  })
+);
